159690b27a2eb45485d2a0a8f08f4d069657c782
Daniel <95697307+Aiyon860@users.noreply.github.com>
2024-01-02 16:32:26 +0700

    Time: 146 ms (5.06%), Space: 51.9 MB (12.76%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -5,11 +5,11 @@
 var maxProfit = function(prices) {
     let left = 0, maxProfit = 0;
     
-    for (let right=1; right < prices.length; right++) {
-        maxProfit = Math.max(maxProfit, prices[right] - prices[left]);
+    for (let right = 1; right < prices.length; right++) {
         if (prices[left] > prices[right]) {
             left = right;
         } 
+        maxProfit = Math.max(maxProfit, prices[right] - prices[left]);
     }
     
     return maxProfit;
