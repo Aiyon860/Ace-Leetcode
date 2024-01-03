b52ee7103e6f05fedefa7e834b0a89613d36f215
Daniel <95697307+Aiyon860@users.noreply.github.com>
2024-01-03 14:21:28 +0700

    Time: 111 ms (5.04%), Space: 44.7 MB (86.49%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.js
+++ 1929-concatenation-of-array/1929-concatenation-of-array.js
@@ -3,13 +3,5 @@
  * @return {number[]}
  */
 var getConcatenation = function(nums) {
-    let ans = [];
-    let n = nums.length;
-    
-    for (let i = 0; i < n; i++) {
-        ans[i] = nums[i];
-        ans[i + n] = nums[i];
-    }
-    
-    return ans;
+    return nums.concat(nums);
 };
