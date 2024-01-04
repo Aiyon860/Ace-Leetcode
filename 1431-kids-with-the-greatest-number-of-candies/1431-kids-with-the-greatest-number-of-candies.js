187a2fa36ababbf38c7dfb31b67fdb408f518c20
Daniel <95697307+Aiyon860@users.noreply.github.com>
2024-01-05 03:01:39 +0700

    Time: 71 ms (12.44%), Space: 42.2 MB (61.08%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.js
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.js
@@ -5,22 +5,18 @@
  */
 var kidsWithCandies = function(candies, extraCandies) {
     let greatestCandies = [];
-    let checkGreatest = 0;
+    let check = 0;
     let n = candies.length;
     
     for (let i = 0; i < n; i++) {
         for (let j = 0; j < n; j++) {
             if (i == j) continue;
             if ((candies[i] + extraCandies) >= candies[j]) {
-                checkGreatest++;
+                check++;
             }
         }
-        if (checkGreatest == (n - 1)) {
-            greatestCandies.push(true);
-        } else {
-            greatestCandies.push(false);
-        }
-        checkGreatest = 0;
+        check == (n - 1) ? greatestCandies.push(true) : greatestCandies.push(false);
+        check = 0;
     }
     
     return greatestCandies;
