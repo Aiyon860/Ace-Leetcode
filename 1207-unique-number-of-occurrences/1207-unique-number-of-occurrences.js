1e78bcc2ef160b9da3f72d12b30fc002d15f85bf
Daniel <95697307+Aiyon860@users.noreply.github.com>
2024-01-04 19:18:29 +0700

    Time: 56 ms (48.33%), Space: 42.4 MB (69.17%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var uniqueOccurrences = function(arr) {
+    let countNums = [], count = 1, checkFalse = 0;
+    
+    if (arr.length == 1) return true;
+    
+    for (let i = 0; i < arr.length; ) {
+        for (let j = 0; j < arr.length; ) {
+            if (arr[i] == arr[j] && i != j) {
+                arr.splice(j, 1);
+                count++;
+            } else {
+                j++;
+            }
+        }
+        countNums.push(count);
+        count = 1;
+        arr.splice(i, 1);
+    }
+    
+    countNums.sort();
+    
+    for (let i = 0; i < countNums.length - 1; i++) {
+        if (countNums[i] == countNums[i + 1]) return false;
+    } 
+    return true;
+};
