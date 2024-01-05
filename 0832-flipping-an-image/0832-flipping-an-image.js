0269011936d29173df76f32317f9f212c6c3ef9b
Daniel <95697307+Aiyon860@users.noreply.github.com>
2024-01-05 20:09:56 +0700

    Time: 58 ms (60.31%), Space: 43.8 MB (61.51%) - LeetHub

--- 0832-flipping-an-image/0832-flipping-an-image.js
+++ 0832-flipping-an-image/0832-flipping-an-image.js
@@ -4,23 +4,16 @@
  */
 var flipAndInvertImage = function(image) {
     let n = image.length;
-    let image_horizontal = [];
     
     for (let i = 0; i < n; i++) {
-        image_horizontal[i] = [];
-    }
-    
-    for (let i = 0; i < n; i++) {
-        for (let j = image[i].length - 1; j >= 0; j--) {
-            if (image[i][j] == 0) {
-                image[i][j] = 1;
-                image_horizontal[i].push(image[i][j]);
-            } else if (image[i][j] == 1) {
-                image[i][j] = 0;
-                image_horizontal[i].push(image[i][j]);
-            }
+        for (let j = 0; j < n; j++) {
+            image[i][j] == 0 ? image[i][j] = 1 : image[i][j] = 0
         }
     }
     
-    return image_horizontal;
+    for (let i = 0; i < n; i++) {
+        image[i].reverse();
+    }
+    
+    return image;
 };
