d3a8ebfce70fe9a28433c3800c9db2ef0c8277b5
Daniel <95697307+Aiyon860@users.noreply.github.com>
2024-01-05 22:08:12 +0700

    Time: 83 ms (51.11%), Space: 48.8 MB (45.40%) - LeetHub

--- 1720-decode-xored-array/1720-decode-xored-array.js
+++ 1720-decode-xored-array/1720-decode-xored-array.js
@@ -4,7 +4,7 @@
  * @return {number[]}
  */
 var decode = function(encoded, first) {
-    let arr = [first]
+    let arr = [first];
     
     for (let i = 0; i < encoded.length; i++) {
         arr[i + 1] = encoded[i] ^ arr[i];
