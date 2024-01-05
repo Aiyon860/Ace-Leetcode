53b922a93b771ddb863ad0926a53e2e45283f628
Daniel <95697307+Aiyon860@users.noreply.github.com>
2024-01-05 20:38:01 +0700

    Time: 59 ms (37.49%), Space: 41.5 MB (98.16%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.js
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.js
@@ -6,7 +6,7 @@ var finalValueAfterOperations = function(operations) {
     let final_value = 0;
     
     for (let i = 0; i < operations.length; i++) {
-        operations[i].includes("--") ? final_value-- : final_value++;
+        operations[i].includes("-") ? final_value-- : final_value++;
     }
     
     return final_value;
