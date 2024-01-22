c48fe22f209d48e53ca08655abf2a1888577fac2
Daniel <95697307+Aiyon860@users.noreply.github.com>
2024-01-22 13:50:23 +0700

    Time: 98 ms (30.89%), Space: 55.8 MB (5.06%) - LeetHub

--- 2788-split-strings-by-separator/2788-split-strings-by-separator.js
+++ 2788-split-strings-by-separator/2788-split-strings-by-separator.js
@@ -7,15 +7,15 @@ var splitWordsBySeparator = function(words, separator) {
     let resultString = [];
     
     for (let i = 0; i < words.length; i++) {
-        if (words[i].includes(separator)) {
+        if (!(words[i].includes(separator))) {
+            resultString.push(words[i]);
+        } else {
             let temp = words[i].split(separator);
             for (let i of temp) {
                 if (i !== '') {
                     resultString.push(i);                
                 }
             }
-        } else {
-            resultString.push(words[i]);
         }
     }
     
