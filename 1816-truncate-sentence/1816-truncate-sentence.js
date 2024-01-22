a2376c8bb025e8b33a3fe63285c6b9c63b14c41a
Daniel <95697307+Aiyon860@users.noreply.github.com>
2024-01-22 11:51:40 +0700

    Time: 54 ms (46.58%), Space: 48.9 MB (5.03%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.js
+++ 1816-truncate-sentence/1816-truncate-sentence.js
@@ -4,12 +4,5 @@
  * @return {string}
  */
 var truncateSentence = function(s, k) {
-    let newSentence = []; 
-    s = s.split(' ');
-    
-    for (let i = 1; i <= k; i++) {
-        newSentence.push(s[i - 1]);
-    }
-    
-    return newSentence.join(' ');;
+    return s.split(' ').slice(0, k).join(' ');
 };
