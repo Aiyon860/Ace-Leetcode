5880ac3a34313e06d2bb0865261c92ab2990b524
Daniel <95697307+Aiyon860@users.noreply.github.com>
2024-01-23 18:23:45 +0700

    Time: 45 ms (87.22%), Space: 48.7 MB (5.26%) - LeetHub

---
+++ 1037-valid-boomerang/1037-valid-boomerang.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[][]} points
+ * @return {boolean}
+ */
+var isBoomerang = function(points) {
+    let [[x1, y1], [x2, y2], [x3, y3]] = points; 
+    
+    return (y2 - y1) * (x3 - x2) != (x2 - x1) * (y3 - y2); 
+};
