1e83e843ccbc4c2bf43ccb4c8210146eb53cac50
Daniel <95697307+Aiyon860@users.noreply.github.com>
2024-01-24 19:39:05 +0700

    Time: 55 ms (59.88%), Space: 50.3 MB (6.98%) - LeetHub

---
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} coordinates
+ * @return {boolean}
+ */
+var checkStraightLine = function(coordinates) {
+    let xDiff = coordinates[1][0] - coordinates[0][0];
+    let yDiff = coordinates[1][1] - coordinates[0][1];
+    let xCurrDiff, yCurrDiff;
+    
+    for (let i = 2; i < coordinates.length; i++) {
+        xCurrDiff = coordinates[i][0] - coordinates[i - 1][0];
+        yCurrDiff = coordinates[i][1] - coordinates[i - 1][1];
+        
+        if ((yDiff * xCurrDiff) !== (xDiff * yCurrDiff)) {
+            return false;
+        }
+    }
+    return true;
+};
