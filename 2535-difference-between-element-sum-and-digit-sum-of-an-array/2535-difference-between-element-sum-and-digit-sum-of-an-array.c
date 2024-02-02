0afccb403d5bc08c9404f94428ab7fa539f8303c
Daniel <95697307+Aiyon860@users.noreply.github.com>
2024-02-02 19:22:19 +0700

    Time: 6 ms (97.46%), Space: 5.9 MB (98.55%) - LeetHub

--- 2535-difference-between-element-sum-and-digit-sum-of-an-array/2535-difference-between-element-sum-and-digit-sum-of-an-array.c
+++ 2535-difference-between-element-sum-and-digit-sum-of-an-array/2535-difference-between-element-sum-and-digit-sum-of-an-array.c
@@ -2,9 +2,9 @@ int digitSum(int number);
 int abs(int number);
 
 int differenceOfSum(int* nums, int numsSize) {    
-    uint32_t element = 0;
-    uint32_t digit = 0;
-    uint32_t absDiff;
+    int element = 0;
+    int digit = 0;
+    int absDiff;
     
     if (numsSize == 1) return 0;
     
