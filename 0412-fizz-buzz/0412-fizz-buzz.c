0a56036c89b45936ee27b4397107a41dbd116d62
Daniel <95697307+Aiyon860@users.noreply.github.com>
2024-02-04 02:49:22 +0700

    Time: 3 ms (94.40%), Space: 6.8 MB (87.20%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.c
@@ -0,0 +1,20 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+char** fizzBuzz(int n, int* returnSize) {
+    char** ans = malloc(n * sizeof(char*));
+    
+    for (int i = 1; i <= n; i++) {
+        if (i % 3 == 0 && i % 5 == 0) ans[i - 1] = "FizzBuzz";
+        else if (i % 3 == 0) ans[i - 1] = "Fizz";
+        else if (i % 5 == 0) ans[i - 1] = "Buzz";
+        else {
+            ans[i - 1] = malloc(6);
+            sprintf(ans[i - 1], "%d", i);            
+        }
+    }
+    
+    *returnSize = n;
+    
+    return ans;
+}
