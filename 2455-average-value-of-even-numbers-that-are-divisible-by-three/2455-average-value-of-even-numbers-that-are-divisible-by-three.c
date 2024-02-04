c8268c0ec90db24691d6c04b1de9ff0233e6364f
Daniel <95697307+Aiyon860@users.noreply.github.com>
2024-02-04 22:53:33 +0700

    Time: 9 ms (41.86%), Space: 6.1 MB (88.37%) - LeetHub

--- 2455-average-value-of-even-numbers-that-are-divisible-by-three/2455-average-value-of-even-numbers-that-are-divisible-by-three.c
+++ 2455-average-value-of-even-numbers-that-are-divisible-by-three/2455-average-value-of-even-numbers-that-are-divisible-by-three.c
@@ -1,17 +1,17 @@
 int averageValue(int* nums, int numsSize) {
-    int ans = 0;
+    int sum = 0;
     int count = 0;
     
     for (int i = 0; i < numsSize; i++) {
         if (nums[i] % 2 == 0 && nums[i] % 3 == 0) {
-            ans += nums[i];
-            count += 1;
+            sum += nums[i];
+            count++;
         }
     }
     
     if (count != 0) {
-        return ans / count;
+        return sum / count;
     } else {
-        return ans;
+        return sum;
     }
 }
