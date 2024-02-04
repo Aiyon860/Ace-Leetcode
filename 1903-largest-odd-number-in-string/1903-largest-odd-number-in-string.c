74e7bb5047fa8d0cda57f300ba56feee71508cbb
Daniel <95697307+Aiyon860@users.noreply.github.com>
2024-02-04 14:19:57 +0700

    Time: 6 ms (81.59%), Space: 9.7 MB (36.20%) - LeetHub

---
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.c
@@ -0,0 +1,16 @@
+char* largestOddNumber(char* num) {
+    int length = strlen(num);
+    char* empty = "";
+    
+    for (int i = length - 1; i >= 0; i--) {
+        if (num[i] % 2 == 1) {
+            char* odd_largest = malloc((i + 2) * sizeof(char));
+            
+            strncpy(odd_largest, num, i + 1);
+            odd_largest[i + 1] = '\0';
+            return odd_largest;
+        }
+    }
+    
+    return empty;
+}
