acce80286a1cc85438486e976adfbab3d01e2a33
Daniel <95697307+Aiyon860@users.noreply.github.com>
2024-02-04 14:48:06 +0700

    Time: 12 ms (24.41%), Space: 8.7 MB (98.38%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.c
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.c
@@ -1,14 +1,20 @@
 char* largestOddNumber(char* num) {
     int length = strlen(num);
+    int largest_odd_index = -1;
     
     for (int i = length - 1; i >= 0; i--) {
         if ((num[i] - '0') % 2 == 1) {
-            memmove(num, num + 0, i + 2);
-            num[i + 1] = '\0';
-            return num;
+            largest_odd_index = i;
+            break;
         }
     }
     
-    num[0] = '\0';
+    if (largest_odd_index != -1) {
+        memmove(num, num + 0, largest_odd_index + 2);
+        num[largest_odd_index + 1] = '\0';
+    } else {
+        num[0] = '\0';
+    }
+    
     return num;
 }
