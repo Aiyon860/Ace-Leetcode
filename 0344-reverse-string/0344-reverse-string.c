e3b367f7a24820bb20fa6324b5ac9ef1af721989
Daniel <95697307+Aiyon860@users.noreply.github.com>
2024-02-06 02:04:01 +0700

    Time: 48 ms (46.80%), Space: 12.4 MB (75.39%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.c
+++ 0344-reverse-string/0344-reverse-string.c
@@ -1,12 +1,10 @@
 void reverseString(char* s, int sSize) {
-    int start = 0;
     int end = sSize - 1;
     
-    for (int i = 0; i < end; i++) {
-        int temp = s[i];
-        s[i] = s[end];
+    for (int start = 0; start < end; start++) {
+        char temp = s[start];
+        s[start] = s[end];
         s[end] = temp;
-        start++;
         end--;
     }
 }
