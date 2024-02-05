0171943900d756c7cfe1e8f6c99b7b1f170c1419
Daniel <95697307+Aiyon860@users.noreply.github.com>
2024-02-06 01:41:34 +0700

    Time: 0 ms (100.00%), Space: 5.5 MB (84.88%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.c
+++ 0507-perfect-number/0507-perfect-number.c
@@ -3,7 +3,7 @@ bool checkPerfectNumber(int num) {
     
     int sum = 1;
     
-    for (int i = 2; i <= sqrt(num); i++) {
+    for (int i = 2; i < sqrt(num); i++) {
         if (num % i == 0) {
             sum += i + (num / i);
         }
