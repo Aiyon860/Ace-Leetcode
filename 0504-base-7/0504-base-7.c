9d38e46d080191908308409b995d330ac8466448
Daniel <95697307+Aiyon860@users.noreply.github.com>
2024-02-05 13:29:44 +0700

    Time: 4 ms (8.25%), Space: 5.6 MB (82.47%) - LeetHub

--- 0504-base-7/0504-base-7.c
+++ 0504-base-7/0504-base-7.c
@@ -17,26 +17,26 @@ char* convertToBase7(int num) {
     int temp = num;
     int length = 0;
     
-    while (temp != 0) {
-        length++;
-        temp /= 7;
-    }
+    do {
+       length++;
+    } while (temp /= 7);
     
     // allocate memory for the result string
     char* ans = (char*)malloc((length + 2) * sizeof(char)); // +2 for negative sign and null-terminator 
     
     int end = length + negative - 1;
-    
+
     if (negative) {
         ans[0] = '-';
     }
-    
-    while (num != 0) {
+
+    // directly assigning the value from (last index - 1)   
+    do {
         ans[end] = (num % 7) + '0';
-        num /= 7;
         end--;
-    }
+    } while (num /= 7);
     
+    // add the null-terminator to the last index
     ans[length + negative] = '\0';
 
     return ans;
