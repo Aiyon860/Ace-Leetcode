f4f1e7a128a404e710771b8ed71d44478e1a4a2a
Daniel <95697307+Aiyon860@users.noreply.github.com>
2024-02-06 16:10:44 +0700

    Time: 3 ms (39.77%), Space: 5.4 MB (97.73%) - LeetHub

--- 0917-reverse-only-letters/0917-reverse-only-letters.c
+++ 0917-reverse-only-letters/0917-reverse-only-letters.c
@@ -1,21 +1,28 @@
 char* reverseOnlyLetters(char* s) {
-    int left = 0;
-    int right = strlen(s) - 1;
+    char* left = s;
+    char* right = s;
+    
+    while (*right) {
+        right++;
+    }
+    right--;
     
     while (left < right) {
-        if (!isalpha(s[left])) {
+        if (!((*left >= 'a' && *left <= 'z') 
+            || (*left >= 'A' && *left <= 'Z'))) {
             left++;
             continue;
         }
         
-        if (!isalpha(s[right])) {
+        if (!((*right >= 'a' && *right <= 'z') 
+            || (*right >= 'A' && *right <= 'Z'))) {
             right--;
             continue;
         }
         
-        int temp = s[left];
-        s[left] = s[right];
-        s[right] = temp;
+        char temp = *left;
+        *left = *right;
+        *right = temp;
 
         left++;
         right--;
