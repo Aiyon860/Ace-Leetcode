14e0b00842b6a1769d3a6c571300fdfe677dfcdb
Daniel <95697307+Aiyon860@users.noreply.github.com>
2024-02-07 19:20:22 +0700

    Time: 4 ms (12.00%), Space: 5.8 MB (68.00%) - LeetHub

--- 0925-long-pressed-name/0925-long-pressed-name.c
+++ 0925-long-pressed-name/0925-long-pressed-name.c
@@ -1,6 +1,13 @@
 bool isLongPressedName(char* name, char* typed) {
-    int n = strlen(name); 
-    int t = strlen(typed);
+    int n = 0;
+    while (name[n] != '\0') {
+        n++;
+    }
+    
+    int t = 0;
+    while (typed[t] != '\0') {
+        t++;
+    }
     
     int i = 0;  // name pointer
     int j = 0;  // typed pointer
