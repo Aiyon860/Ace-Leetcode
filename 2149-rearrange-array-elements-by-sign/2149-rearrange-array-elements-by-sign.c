d33c46e7ba2c7c75fa21d1e0fd6605a224d68734
Daniel <95697307+Aiyon860@users.noreply.github.com>
2024-02-09 01:39:08 +0700

    Time: 672 ms (96.67%), Space: 99.4 MB (85.00%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.c
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.c
@@ -3,31 +3,18 @@
  */
 int* rearrangeArray(int* nums, int numsSize, int* returnSize) {
     int* ans = (int*) malloc(numsSize * sizeof(int));
-    int* arr_pos = (int*) malloc(numsSize / 2 * sizeof(int));
-    int* arr_neg = (int*) malloc(numsSize / 2 * sizeof(int));
     
-    int a = 0;
-    int b = 0;
+    int pos = 0;
+    int neg = 1;
     
     for (int i = 0; i < numsSize; i++) {
         if (nums[i] > 0) {
-            arr_pos[a] = nums[i];
-            a++;
+            ans[pos] = nums[i];
+            pos += 2;
         } else {
-            arr_neg[b] = nums[i];
-            b++;
+            ans[neg] = nums[i];
+            neg += 2;
         }
-    }
-    
-    a = 0;
-    b = 0;
-    
-    for (int i = 0; i < numsSize; i += 2) {
-        ans[i] = arr_pos[a];
-        ans[i + 1] = arr_neg[b];
-        
-        a++;
-        b++;
     }
     
     *returnSize = numsSize;
