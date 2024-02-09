0b1675f5d1571a2d86887788c3855d6d1db20099
Daniel <95697307+Aiyon860@users.noreply.github.com>
2024-02-10 00:45:50 +0700

    Time: 29 ms (85.45%), Space: 10.4 MB (81.82%) - LeetHub

---
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.c
@@ -0,0 +1,26 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* decompressRLElist(int* nums, int numsSize, int* returnSize) {
+    // calculate the decompressed array
+    int size = 0;
+    for (int i = 0; i < numsSize; i += 2) {
+        size += nums[i];
+    }
+    
+    // allocate memory for the decompressed array
+    int* ans = (int*) malloc(size * sizeof(int));
+    int a = 0;
+    
+    // decompress the RLE list
+    for (int i = 0; i < numsSize; i += 2) {
+        for (int j = 0; j < nums[i]; j++) {
+            ans[a] = nums[i + 1];
+            a++;
+        }
+    }
+    
+    *returnSize = size;
+    
+    return ans;
+}
