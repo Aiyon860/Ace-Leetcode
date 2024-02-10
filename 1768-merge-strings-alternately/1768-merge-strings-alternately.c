2c6009c344664b817cc646ee8e5305830296adc8
Daniel <95697307+Aiyon860@users.noreply.github.com>
2024-02-11 02:42:25 +0700

    Time: 0 ms (100.00%), Space: 5.8 MB (75.61%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.c
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.c
@@ -4,22 +4,19 @@ char* mergeAlternately(char * word1, char * word2){
     int len = len1 + len2;
     char* ans = (char*) malloc((len + 1) * sizeof(char));
     
-    int i, j, k;
-    for (i = 0, j = 0, k = 0; j < len1 && k < len2; i += 2, j++, k++) {
-        ans[i] = word1[j];
-        ans[i + 1] = word2[k];
+    int i = 0, j = 0, k = 0;
+    while (j < len1 && k < len2) {
+        ans[i] = word1[j++];
+        ans[i + 1] = word2[k++];
+        i += 2;
     }
     
     while (j < len1) {
-        ans[i] = word1[j];
-        i++;
-        j++;
+        ans[i++] = word1[j++];
     }
     
     while (k < len2) {
-        ans[i] = word2[k];
-        i++;
-        k++;
+        ans[i++] = word2[k++];
     }
     
     ans[i] = '\0';
