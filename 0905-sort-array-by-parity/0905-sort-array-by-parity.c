7cf4ef50dc3204c0150edfaeb625bd228e097b03
Daniel <95697307+Aiyon860@users.noreply.github.com>
2024-02-12 11:26:24 +0700

    Time: 24 ms (76.58%), Space: 9 MB (76.58%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.c
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.c
@@ -2,24 +2,25 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* sortArrayByParity(int* nums, int numsSize, int* returnSize) {
-    int* ans = (int*) malloc(numsSize * sizeof(int));
-    
-    int j = 0;
-    for (int i = 0; i < numsSize; i++) {
-        if (nums[i] % 2 == 0) {
-            ans[j] = nums[i];
-            j++;
-        }
-    }
-    
-    for (int i = 0; i < numsSize; i++) {
-        if (nums[i] % 2 == 1) {
-            ans[j] = nums[i];
-            j++;
+    int left = 0;               // left pointer
+    int right = numsSize - 1;   // right pointer
+        
+    while (left < right) {
+        if (nums[left] % 2 == 0) {
+            left++;
+        } else if (nums[right] % 2 == 0) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            
+            left++;
+            right--;
+        } else {
+            right--;
         }
     }
     
     *returnSize = numsSize;
     
-    return ans;
+    return nums;
 }
