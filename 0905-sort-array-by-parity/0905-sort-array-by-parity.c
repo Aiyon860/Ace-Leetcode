0a406948f90407d261e499f3f34a71d4c3471386
Daniel <95697307+Aiyon860@users.noreply.github.com>
2024-02-12 11:31:39 +0700

    Time: 28 ms (46.85%), Space: 8.7 MB (93.24%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.c
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.c
@@ -5,11 +5,15 @@ int* sortArrayByParity(int* nums, int numsSize, int* returnSize) {
     uint16_t left = 0;               // left pointer
     uint16_t right = numsSize - 1;   // right pointer
         
-    // do the iteration until left == right or left > right
+    // do the iteration until left == right or left > right.
     while (left < right) {
-        // 
+        
+        // if the left index's value is already an even number.
         if (nums[left] % 2 == 0) {
             left++;
+            
+        // if the left index's value is an odd number and
+        // the right index's value is an even number, then swap.
         } else if (nums[right] % 2 == 0) {
             int temp = nums[left];
             nums[left] = nums[right];
@@ -17,6 +21,8 @@ int* sortArrayByParity(int* nums, int numsSize, int* returnSize) {
             
             left++;
             right--;
+        
+        // 
         } else {
             right--;
         }
