b88c46e27738dbb1daf422c7c6f279330de7ad53
Daniel <95697307+Aiyon860@users.noreply.github.com>
2024-12-01 20:41:13 +0700

    Time: 0 ms (100%), Space: 8.5 MB (5.34%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
+    unordered_map<int, int> mp = {{0,0}, {1,1}};
     int fib(int n) {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-        vector<int> a = {0, 1};
-        while (n > 1) {
-            int temp = a[0];
-            a[0] = a[1];
-            a[1] += temp;
-            --n;
-        }
-        return a[1];
+        if (n <= 1) return n;
+        if (mp.count(n)) return mp[n];
+        mp[n] = fib(n - 2) + fib(n - 1); 
+        return mp[n];
     }
 };
